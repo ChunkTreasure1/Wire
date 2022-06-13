7d1b61852421a96bf0e6b03711b0398fbc19b2f9
Ivar Jönsson <chunktreasure@gmail.com>
2022-06-14 01:06:27 +0200

    Changed REGISTER_COMPONENT usage and functionality

--- Wire/src/Wire/ECS/Serialization.cpp
+++ Wire/src/Wire/ECS/Serialization.cpp
@@ -5,11 +5,48 @@
 
 namespace Wire
 {
-	bool ComponentRegistry::Register(const std::string& name, const RegistrationInfo& guid)
+	namespace Utility
+	{
+		static ComponentRegistry::PropertyType PropertyFromString(const std::string& string)
+		{
+			if (string == "std::string") return ComponentRegistry::PropertyType::String;
+			if (string == "bool") return ComponentRegistry::PropertyType::Bool;
+
+			if (string == "int") return ComponentRegistry::PropertyType::Int;
+			if (string == "int32_t") return ComponentRegistry::PropertyType::Int;
+
+			if (string == "unsigned int") return ComponentRegistry::PropertyType::UInt; // TODO: Implement this
+			if (string == "uint32_t") return ComponentRegistry::PropertyType::UInt;
+
+			if (string == "short") return ComponentRegistry::PropertyType::Short;
+			if (string == "int16_t") return ComponentRegistry::PropertyType::Short;
+
+			if (string == "unsigned short") return ComponentRegistry::PropertyType::UShort; // TODO: Implement this
+			if (string == "uint16_t") return ComponentRegistry::PropertyType::UShort;
+
+			if (string == "char") return ComponentRegistry::PropertyType::Char;
+			if (string == "int8_t") return ComponentRegistry::PropertyType::Char;
+
+			if (string == "unsigned char") return ComponentRegistry::PropertyType::UChar; // TODO: Implement this
+			if (string == "uint8_t") return ComponentRegistry::PropertyType::UChar;
+
+			if (string == "float") return ComponentRegistry::PropertyType::Float;
+			if (string == "double") return ComponentRegistry::PropertyType::Double;
+
+			if (string == "glm::vec2") return ComponentRegistry::PropertyType::Vector2;
+			if (string == "glm::vec3") return ComponentRegistry::PropertyType::Vector3;
+			if (string == "glm::vec4") return ComponentRegistry::PropertyType::Vector4;
+
+			return ComponentRegistry::PropertyType::Unknown;
+		}
+	}
+
+	bool ComponentRegistry::Register(const std::string& name, const std::string& definitionData, const RegistrationInfo& guid)
 	{
 		if (auto it = ComponentGUIDs().find(name); it == ComponentGUIDs().end())
 		{
 			ComponentGUIDs()[name] = guid;
+			ParseDefinition(definitionData, ComponentGUIDs()[name]);
 			return true;
 		}
 
@@ -45,7 +82,48 @@ namespace Wire
 		static std::unordered_map<std::string, RegistrationInfo> impl;
 		return impl;
 	}
-	
+
+
+	/*
+	struct TestComponent
+	{
+		uint32_t dataOne;
+		uint32_t dataTwo;
+
+		inline static constexpr WireGUID comp_guid;
+	};
+	*/
+
+	void ComponentRegistry::ParseDefinition(const std::string& definitionData, RegistrationInfo& outInfo)
+	{
+		// Find first {, which is the start of the component
+		auto it = definitionData.find('{');
+
+		while (it != std::string::npos)
+		{
+			it = definitionData.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", it);
+			if (it == std::string::npos)
+			{
+				break;
+			}
+
+			auto spaceIt = definitionData.find_first_of(' ', it);
+			PropertyType type = Utility::PropertyFromString(definitionData.substr(it, spaceIt - it));
+
+			auto nameEndIt = definitionData.find_first_of(";", spaceIt);
+			std::string name = definitionData.substr(spaceIt + 1, nameEndIt - spaceIt - 1);
+
+			if (type != PropertyType::Unknown)
+			{
+				auto& property = outInfo.properties.emplace_back();
+				property.name = name;
+				property.type = type;
+			}
+
+			it = definitionData.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", nameEndIt);
+		}
+	}
+
 	void Serializer::SerializeEntityToFile(EntityId aId, const Registry& aRegistry, const std::filesystem::path& aSceneFolder)
 	{
 		std::vector<uint8_t> data;
@@ -53,11 +131,11 @@ namespace Wire
 		const uint32_t componentCount = aRegistry.GetComponentCount(aId);
 
 		data.resize(sizeof(EntityId) + sizeof(uint32_t) + encodedComponentData.size());
-		
+
 		memcpy_s(data.data(), sizeof(EntityId), &aId, sizeof(EntityId));
 		memcpy_s(&data[sizeof(EntityId)], sizeof(uint32_t), &componentCount, sizeof(uint32_t));
 		memcpy_s(&data[sizeof(EntityId) + sizeof(uint32_t)], encodedComponentData.size(), encodedComponentData.data(), encodedComponentData.size());
-	
+
 		if (!std::filesystem::exists(aSceneFolder))
 		{
 			std::filesystem::create_directories(aSceneFolder);
@@ -69,7 +147,7 @@ namespace Wire
 		file.write(reinterpret_cast<char*>(data.data()), data.size());
 		file.close();
 	}
-	
+
 	EntityId Serializer::DeserializeEntityToRegistry(const std::filesystem::path& aPath, Registry& aRegistry)
 	{
 		std::ifstream file(aPath.string(), std::ios::binary);
@@ -93,18 +171,18 @@ namespace Wire
 
 		const uint32_t componentCount = *reinterpret_cast<uint32_t*>(&totalData[offset]);
 		offset += sizeof(uint32_t);
-		
+
 		for (uint32_t i = 0; i < componentCount; i++)
 		{
 			const uint16_t nameSize = *reinterpret_cast<uint16_t*>(&totalData[offset]);
 			offset += sizeof(uint16_t);
-			
+
 			std::string name(reinterpret_cast<char*>(&totalData[offset]), nameSize);
 
 			offset += nameSize;
 
 			const ComponentRegistry::RegistrationInfo& registryData = ComponentRegistry::GetRegistryDataFromName(name);
-			
+
 			std::vector<uint8_t> componentData;
 			componentData.resize(registryData.size);
 			memcpy_s(componentData.data(), componentData.size(), &totalData[offset], registryData.size);
@@ -112,7 +190,7 @@ namespace Wire
 
 			aRegistry.AddComponent(componentData, registryData.guid, id);
 		}
-		
+
 		return id;
 	}
 }
--- Wire/src/Wire/ECS/Serialization.h
+++ Wire/src/Wire/ECS/Serialization.h
@@ -7,26 +7,57 @@
 #include <filesystem>
 
 #define SERIALIZE_COMPONENT(type, guid) inline static constexpr WireGUID comp_guid = guid;
-#define REGISTER_COMPONENT(type) inline static bool type##_reg = Wire::ComponentRegistry::Register(#type, { type::comp_guid, sizeof(type) });
+
+#define REGISTER_COMPONENT(definition, type) definition; \
+inline static bool type##_reg = Wire::ComponentRegistry::Register(#type, #definition, { type::comp_guid, sizeof(type) });
 
 namespace Wire
 {
 	class ComponentRegistry
 	{
 	public:
+		enum class PropertyType : uint32_t
+		{
+			Bool = 0,
+			Int = 1,
+			UInt = 2,
+			Short = 3,
+			UShort = 4,
+			Char = 5,
+			UChar = 6,
+			Float = 7,
+			Double = 8,
+			Vector2 = 9,
+			Vector3 = 10,
+			Vector4 = 11,
+			String = 12,
+			Unknown = 13
+		};
+
+		struct ComponentProperty
+		{
+			std::string name;
+			PropertyType type;
+		};
+
 		struct RegistrationInfo
 		{
 			WireGUID guid = WireGUID::Null();
 			size_t size = 0;
+
+			std::vector<ComponentProperty> properties;
 		};
 
 		ComponentRegistry() = delete;
 
-		static bool Register(const std::string& name, const RegistrationInfo& guid);
+		static bool Register(const std::string& name, const std::string& definitionData, const RegistrationInfo& guid);
 		static const std::string GetNameFromGUID(const WireGUID& aGuid);
 		static const RegistrationInfo& GetRegistryDataFromName(const std::string& aName);
 
 		static std::unordered_map<std::string, RegistrationInfo>& ComponentGUIDs();
+
+	private:
+		static void ParseDefinition(const std::string& definitionData, RegistrationInfo& outInfo);
 	};
 
 	class Serializer
